04677d7c77f360354cd8f3276f75e2d340d9942b
sandaruwannaveen123-byte <sandaruwannaveen123@gmail.com>
2026-07-15 11:08:39 +0100

    Updated phone number

--- src/components/WhatsApp.jsx
+++ src/components/WhatsApp.jsx
@@ -4,7 +4,7 @@ import { FaWhatsapp } from "react-icons/fa";
 function WhatsApp() {
   return (
     <a
-      href="https://wa.me/971501234567"
+      href="https://wa.me/971561234567"
       className="whatsapp"
       target="_blank"
       rel="noopener noreferrer"
